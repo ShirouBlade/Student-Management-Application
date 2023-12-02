42c1a5c223a3b39f0149f7b0660176574d8996b7
HongVy <vylevohong@gmail.com>
2023-12-02 15:38:14 -0800

    Create ErrorMessage.js

---
+++ frontend/src/components/Display Message/ErrorMessage.js
@@ -0,0 +1,12 @@
+/*checked*/
+import React from 'react';
+
+const ErrorMessage = ({ error }) => {
+  return (
+    <div class='alert alert-warning' role='alert'>
+      {error}
+    </div>
+  );
+};
+
+export default ErrorMessage;
