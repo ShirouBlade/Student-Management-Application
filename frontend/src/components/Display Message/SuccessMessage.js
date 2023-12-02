ee28ee68ac321cd04d52a27c21fe6ad609b6f81d
HongVy <vylevohong@gmail.com>
2023-12-02 15:38:34 -0800

    Add files via upload

---
+++ frontend/src/components/Display Message/SuccessMessage.js
@@ -0,0 +1,12 @@
+/*checked*/
+import React from 'react';
+
+const SuccessMessage = ({ msg }) => {
+  return (
+    <div className='alert alert-success' role='alert'>
+      {msg}
+    </div>
+  );
+};
+
+export default SuccessMessage;
