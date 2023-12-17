75a39dca701df8834350e2835dd88e94fe37a5df
ShirouBlade <111835719+ShirouBlade@users.noreply.github.com>
2023-12-17 12:59:25 -0800

    Add files via upload

--- backend/routes/studentsRoute.js
+++ backend/routes/studentsRoute.js
@@ -9,18 +9,19 @@ const studentsRoute = express.Router();
 
 //Register
 studentsRoute.post('/register', asyncHandler(async(req, res) =>{
-    const {name, email, password} = req.body;
+    const {firstName, lastName, email, password} = req.body;
     const studentExists = await Student.findOne({email: email});
     if(userExists){
         throw new Error('Student Exists already');
     }
-    const studentCreated = await Student.create({name, email, password});
+    const studentCreated = await Student.create({firstName, lastName, email, password});
     res.send(userCreated);
     //set status code
     res.status(200);
     res.json({
         _id: studentCreated._id,
-        name: studentCreated.name,
+        firstName: studentCreated.firstName,
+        lastName: studentCreated.lastName,
         password: studentCreated.password,
         email: studentCreated.password,
         token: generateToken(student._id),
@@ -37,7 +38,8 @@ studentsRoute.post('/login', asyncHandler(async(req, res) =>{
         res.status(200);
         res.json({
             _id: student._id,
-            name: student.name,
+            firstName: student.firstName,
+            lastName: student.lastName,
             password: student.password,
             email: student.password,
             token: generateToken(student._id),
@@ -49,18 +51,51 @@ studentsRoute.post('/login', asyncHandler(async(req, res) =>{
 }));
 
 //Update User
-studentsRoute.put('/update', authMiddleware, (req, res) =>{
-    res.send('Update Route');
-});
+studentsRoute.put('/update', authMiddleware, asyncHandler( async (req, res) => {
+    const student = await Student.findById(req.student._id);
+    if(student){
+        student.firstName = req.body.firstName || student.firstName;
+        student.lastName = req.body.lastName || student.lastName;
+        student.email = req.body.email || student.email;
+        if(req.body.password){
+            student.password = req.body.password || student.password;
+        }
+
+        const updatedStudent = await student.save();
+
+        res.json({
+            _id: updatedStudent._id,
+            firstName: updatedStudent.firstName,
+            lastName: updatedStudent.lastName,
+            email: updatedStudent.email,
+            token: generateToken(updatedStudent._id),
+        });
+    }
+}));
 
 //Fetch Users
-studentsRoute.get('/', authMiddleware, (req, res)=>{
-    console.log(req.headers);
-    res.send('Fetch students');
-});
+studentsRoute.get('/', asyncHandler(async(req, res)=>{
+    const student = await Student.findById(req.student._id);
+
+    if(student){
+        res.status(200);
+        res.json(student);
+    }else{
+        res.status(500);
+        throw new Error('There are no Events');
+    }
+
+}));
 
 //Delete User
-studentsRoute.delete('/:id', (req, res) => {
-    res.send('Delete Route');
-});
+studentsRoute.delete('/:id', asyncHandler(async (req, res) => {
+    try {
+        const student = await Student.findByIdAndDelete(req.params.id);
+
+        res.status(200);
+        res.send(student);
+    } catch (error) {
+        res.json(error);
+    }
+}));
 module.exports = studentsRoute;
