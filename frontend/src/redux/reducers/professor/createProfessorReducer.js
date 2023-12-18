2887ed919ef9646d7fa1da36ab85d375e824fcff
MicDaryll <152748962+MicDaryll@users.noreply.github.com>
2023-12-18 07:04:01 -0800

    Create createProfessorReducer.js

---
+++ frontend/src/redux/reducers/professor/createProfessorReducer.js
@@ -0,0 +1,29 @@
+import {
+    
+    CREATE_PROFESSORDATA_REQUEST, 
+    CREATE_PROFESSORDATA_FAIL, 
+    CREATE_PROFESSORDATA_SUCCESS 
+
+} from '../../actions/datatypes';
+
+const CreateProfessorReducers = (state={}, action) => {
+    switch(action.type){
+        case CREATE_PROFESSORDATA_REQUEST:
+            return{
+                loading: true,
+            };
+        case CREATE_PROFESSORDATA_SUCCESS:
+            return{
+                professordata: action.payload,
+            };
+        case CREATE_PROFESSORDATA_FAIL: 
+            return{
+                loading: false, 
+                error: action.payload, 
+            };
+
+        default: 
+            return state;
+    };
+};
+export{CreateProfessorReducers};
