dc17bfb0364b77f38530613155d142a7051a0216
MicDaryll <152748962+MicDaryll@users.noreply.github.com>
2023-12-18 07:04:47 -0800

    Create professorDetailReducer.js

---
+++ frontend/src/redux/reducers/professor/professorDetailReducer.js
@@ -0,0 +1,28 @@
+import {
+    PROFESSORPROFILE_DETAIL_FAIL,
+    PROFESSORPROFILE_DETAIL_REQUEST,
+    PROFESSORPROFILE_DETAIL_SUCCESS,
+  } from '../../actions/datatypes';
+  
+  const professordetailreducer = (state = {}, action) => {
+    switch (action.type) {
+      case PROFESSORPROFILE_DETAIL_REQUEST:
+        return {
+          loading: true,
+        };
+      case PROFESSORPROFILE_DETAIL_SUCCESS:
+        return {
+          book: action.payload,
+          loading: false,
+        };
+      case PROFESSORPROFILE_DETAIL_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default professordetailreducer;
